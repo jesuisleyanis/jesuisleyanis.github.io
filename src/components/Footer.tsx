65e2ab0823029e899ebad276c4dd0a82cab36c33
Yanis Capelle <capelleyanis@gmail.com>
2024-08-27 21:01:13 +0200

    fix text wrap on mobile version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="bg-black/10 h-32 w-full flex justify-center">
             <div className="w-4/5 lg:w-3/5 text-left flex items-center">  
-                <p className="text-md font-medium">Designed and built by Yanis Capelle</p>
+                <p className="lg:text-md text-xs font-medium">Designed and built by Yanis Capelle</p>
                 <p className="text-3xl mx-2">⸱</p>  
                 <Heart size={16} />  
             </div>
